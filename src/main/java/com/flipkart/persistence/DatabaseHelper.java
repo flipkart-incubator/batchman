b1d551210f9634f8a88856c02e23c02745f4310b
anirudh.r <anirudh.r@flipkart.com>
2016-02-22 15:42:56 +0530

    improved DatabaseHelper

--- src/main/java/com/flipkart/persistence/DatabaseHelper.java
+++ src/main/java/com/flipkart/persistence/DatabaseHelper.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import com.flipkart.data.Data;
 import com.flipkart.exception.DeserializeException;
@@ -20,11 +19,12 @@ import java.util.Collection;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
-    public static final String TABLE_EVENT_DATA = "tableEventData";
-    public static final String KEY_ID = "id";
-    public static final String KEY_DATA = "data";
-    public static final String KEY_EXPIRY = "expiry";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_EVENT_DATA = "tableEventData";
+    private static final String KEY_ID = "id";
+    private static final String KEY_DATA = "data";
+    private static final String KEY_EXPIRY = "expiry";
+    private static final String WHERE_CLAUSE = KEY_ID + " = ?";
     private SerializationStrategy serializationStrategy;
 
     public DatabaseHelper(SerializationStrategy serializationStrategy, String databaseName, Context context) {
@@ -58,17 +58,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public void addData(Collection<Data> dataCollection) throws SerializeException {
         SQLiteDatabase db = this.getWritableDatabase();
-        for (Data data : dataCollection) {
-            if (!isDataInDB(String.valueOf(data.getEventId()))) {
-                ContentValues values = new ContentValues();
-                values.put(KEY_ID, data.getEventId());
-                values.put(KEY_DATA, serializationStrategy.serializeData(data));
-                values.put(KEY_EXPIRY, 0);
-                db.insert(TABLE_EVENT_DATA, null, values);//todo : transactions use
+        try {
+            db.beginTransaction();
+            for (Data data : dataCollection) {
+                if (!isDataInDB(String.valueOf(data.getEventId()))) {
+                    ContentValues values = new ContentValues();
+                    values.put(KEY_ID, data.getEventId());
+                    values.put(KEY_DATA, serializationStrategy.serializeData(data));
+                    values.put(KEY_EXPIRY, 0);
+                    db.insertWithOnConflict(TABLE_EVENT_DATA, null, values, SQLiteDatabase.CONFLICT_REPLACE);
+                }
             }
+            db.setTransactionSuccessful();
+        } finally {
+            db.endTransaction();
         }
-        db.close();
-
     }
 
     /**
@@ -77,21 +81,18 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * @return collection of {@link Data} objects
      * @throws DeserializeException
      */
-
     public Collection<Data> getAllData() throws DeserializeException {
         ArrayList<Data> allEventData = new ArrayList<>();
-        String selectQuery = "SELECT * FROM " + TABLE_EVENT_DATA;
         SQLiteDatabase db = getWritableDatabase();
-        Data event;
-            Cursor cursor = db.rawQuery(selectQuery, null);
-            if (cursor.moveToFirst()) {
-                do {
-                    event = serializationStrategy.deserializeData(cursor.getBlob(1));
-                    allEventData.add(event);
-                } while (cursor.moveToNext());
-            }
-            cursor.close();
-            return allEventData;
+        Cursor cursor = db.query(TABLE_EVENT_DATA, new String[]{KEY_DATA}, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            do {
+                Data event = serializationStrategy.deserializeData(cursor.getBlob(0));
+                allEventData.add(event);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return allEventData;
     }
 
     /**
@@ -100,20 +101,23 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      *
      * @param dataCollection collection of {@link Data} objects to be deleted
      */
-
     public void deleteDataList(Collection<Data> dataCollection) {
-        Log.d("db delete", "Deleted data from db " + dataCollection.size());
-
-        SQLiteDatabase db = getWritableDatabase();
+        StringBuilder selectionArg = new StringBuilder();
+        int idx = 0;
         for (Data data : dataCollection) {
-            db.delete(TABLE_EVENT_DATA, KEY_ID + "=" + data.getEventId(), null);
+            if (idx != 0) {
+                selectionArg.append(",");
+            }
+            selectionArg.append(data.getEventId());
+            idx++;
         }
+        SQLiteDatabase db = getWritableDatabase();
+        db.delete(TABLE_EVENT_DATA, KEY_ID + " IN (?) ", new String[]{selectionArg.toString()});
     }
 
     /**
      * This method deletes all the data in the database.
      */
-
     public void deleteAll() {
         SQLiteDatabase db = getWritableDatabase();
         db.delete(TABLE_EVENT_DATA, null, null);
@@ -125,16 +129,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
      * @param id eventId of {@link Data} object
      * @return true is {@link Data} object is in database
      */
-
     public boolean isDataInDB(String id) {
         SQLiteDatabase db = getReadableDatabase();
-        String Query = "Select * from " + TABLE_EVENT_DATA + " where " + KEY_ID + " = " + id;
-        Cursor cursor = db.rawQuery(Query, null);
-        if (cursor.getCount() <= 0) {
-            cursor.close();
-            return false;
-        }
+        Cursor cursor = db.query(TABLE_EVENT_DATA, new String[]{KEY_ID}, WHERE_CLAUSE, new String[]{id}, null, null, null);
+        boolean result = cursor.getCount() > 0;
         cursor.close();
-        return true;
+        return result;
     }
 }
