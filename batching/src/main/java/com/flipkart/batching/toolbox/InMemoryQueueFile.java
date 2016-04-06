e46c18dcc8db6abe513e7a77d7c1c1a3f748d352
kushal.sharma <kushal.sharma@flipkart.com>
2016-04-06 16:39:53 +0530

    added InMemoeryQueue implementation

--- batching/src/main/java/com/flipkart/batching/toolbox/InMemoryQueueFile.java
+++ batching/src/main/java/com/flipkart/batching/toolbox/InMemoryQueueFile.java
@@ -1,44 +1,44 @@
 package com.flipkart.batching.toolbox;
 
-import com.flipkart.batching.Data;
 import com.flipkart.batching.exception.DeserializeException;
 import com.flipkart.batching.exception.SerializeException;
-import com.flipkart.batching.persistence.SerializationStrategy;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Created by kushal.sharma on 06/04/16.
  */
-public class InMemoryQueueFile implements QueueFile {
-    ArrayList<byte[]> inMemoryQueue;
-    SerializationStrategy serializationStrategy;
 
-    public InMemoryQueueFile(SerializationStrategy serializationStrategy) {
-        this.inMemoryQueue = new ArrayList<>();
-        this.serializationStrategy = serializationStrategy;
+public class InMemoryQueueFile implements IQueueFile {
+    LinkedList<byte[]> inMemoryQueue;
+
+    public InMemoryQueueFile() {
+        this.inMemoryQueue = new LinkedList<>();
     }
 
     @Override
-    public void addToQueue(Data data) throws SerializeException, IOException {
-        inMemoryQueue.add(serializationStrategy.serializeData(data));
+    public void add(byte[] data) throws SerializeException, IOException {
+        inMemoryQueue.addLast(data);
     }
 
     @Override
-    public boolean removeFromQueue() throws IOException {
-        if (!inMemoryQueue.isEmpty()) {
-            inMemoryQueue.remove(0);
-            return true;
-        }
-        return false;
+    public void remove() throws IOException {
+        inMemoryQueue.removeFirst();
     }
 
     @Override
-    public Data peekFromQueue() throws IOException, DeserializeException {
-        if (!inMemoryQueue.isEmpty()) {
-            return serializationStrategy.deserializeData(inMemoryQueue.get(0));
-        }
-        return null;
+    public byte[] peek() throws IOException, DeserializeException {
+        return inMemoryQueue.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return inMemoryQueue.size();
+    }
+
+    @Override
+    public void close() throws IOException {
+        // Nothing to close.
     }
 }
