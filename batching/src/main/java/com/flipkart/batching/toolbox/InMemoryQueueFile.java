5dad265dbd2a3c8751862fab22e92bb3329fe5d7
kushal.sharma <kushal.sharma@flipkart.com>
2016-04-11 01:48:55 +0530

    updated in memory queue file

--- batching/src/main/java/com/flipkart/batching/toolbox/InMemoryQueueFile.java
+++ batching/src/main/java/com/flipkart/batching/toolbox/InMemoryQueueFile.java
@@ -4,41 +4,49 @@ import com.flipkart.batching.exception.DeserializeException;
 import com.flipkart.batching.exception.SerializeException;
 
 import java.io.IOException;
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by kushal.sharma on 06/04/16.
  */
 
 public class InMemoryQueueFile implements IQueueFile {
-    LinkedList<byte[]> inMemoryQueue;
+    InMemoryObjectQueue inMemoryObjectQueue;
 
     public InMemoryQueueFile() {
-        this.inMemoryQueue = new LinkedList<>();
+        this.inMemoryObjectQueue = new InMemoryObjectQueue<>();
     }
 
+
     @Override
-    public void add(byte[] data) throws SerializeException, IOException {
-        inMemoryQueue.addLast(data);
+    public void add(Object data) throws SerializeException, IOException {
+        inMemoryObjectQueue.add(data);
     }
 
     @Override
     public void remove() throws IOException {
-        inMemoryQueue.removeFirst();
+        inMemoryObjectQueue.remove();
     }
 
     @Override
-    public byte[] peek() throws IOException, DeserializeException {
-        return inMemoryQueue.getFirst();
+    public Object peek() throws IOException, DeserializeException {
+        return inMemoryObjectQueue.peek();
     }
+
 
     @Override
     public int size() {
-        return inMemoryQueue.size();
+        return inMemoryObjectQueue.size();
     }
 
     @Override
     public void close() throws IOException {
         // Nothing to close.
+    }
+
+    @Override
+    public Collection peek(int size) {
+        return new ArrayList();
     }
 }
