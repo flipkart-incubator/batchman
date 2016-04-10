19c2756f5cc4600a95f116812131835de1b1a154
kushal.sharma <kushal.sharma@flipkart.com>
2016-04-11 01:47:57 +0530

    updated tape queue file

--- batching/src/main/java/com/flipkart/batching/toolbox/TapeQueueFile.java
+++ batching/src/main/java/com/flipkart/batching/toolbox/TapeQueueFile.java
@@ -1,25 +1,33 @@
 package com.flipkart.batching.toolbox;
 
+import com.flipkart.batching.Data;
 import com.flipkart.batching.exception.DeserializeException;
 import com.flipkart.batching.exception.SerializeException;
+import com.flipkart.batching.persistence.SerializationStrategy;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * Created by kushal.sharma on 06/04/16.
  */
 
-public class TapeQueueFile implements IQueueFile {
-    com.squareup.tape.QueueFile queueFile;
+public class TapeQueueFile<E extends Data> implements IQueueFile {
+    QueueFile queueFile;
+    SerializationStrategy serializationStrategy;
 
-    public TapeQueueFile(File file) throws IOException {
-        queueFile = new com.squareup.tape.QueueFile(file);
+    public TapeQueueFile(File file, SerializationStrategy serializationStrategy) throws IOException {
+        queueFile = new QueueFile(file);
+        this.serializationStrategy = serializationStrategy;
     }
 
+
     @Override
-    public void add(byte[] data) throws SerializeException, IOException {
-        queueFile.add(data);
+    public void add(Object data) throws SerializeException, IOException {
+        queueFile.add(serializationStrategy.serializeData((E) data));
     }
 
     @Override
@@ -28,8 +36,8 @@ public class TapeQueueFile implements IQueueFile {
     }
 
     @Override
-    public byte[] peek() throws IOException, DeserializeException {
-        return queueFile.peek();
+    public E peek() throws IOException, DeserializeException {
+        return (E) serializationStrategy.deserializeData(queueFile.peek());
     }
 
     @Override
@@ -40,5 +48,30 @@ public class TapeQueueFile implements IQueueFile {
     @Override
     public void close() throws IOException {
         queueFile.close();
+    }
+
+    @Override
+    public Collection<E> peek(final int size) {
+        final ArrayList<E> dataList = new ArrayList<>(size);
+        try {
+            queueFile.forEach(new QueueFile.ElementVisitor() {
+                int count = 0;
+
+                @Override
+                public boolean read(InputStream in, int length) throws IOException {
+                    byte[] data = new byte[length];
+                    in.read(data, 0, length);
+                    try {
+                        dataList.add((E) serializationStrategy.deserializeData(data));
+                    } catch (DeserializeException e) {
+                        e.printStackTrace();
+                    }
+                    return ++count < size;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataList;
     }
 }
