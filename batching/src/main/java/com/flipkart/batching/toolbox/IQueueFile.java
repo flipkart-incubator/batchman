f25bc8581b6af4ef787e84433b9a0125eca2469a
kushal.sharma <kushal.sharma@flipkart.com>
2016-04-11 01:46:13 +0530

    updated interface

--- batching/src/main/java/com/flipkart/batching/toolbox/IQueueFile.java
+++ batching/src/main/java/com/flipkart/batching/toolbox/IQueueFile.java
@@ -4,18 +4,21 @@ import com.flipkart.batching.exception.DeserializeException;
 import com.flipkart.batching.exception.SerializeException;
 
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Created by kushal.sharma on 06/04/16.
  */
-public interface IQueueFile {
-    void add(byte[] data) throws SerializeException, IOException;
+public interface IQueueFile<E> {
+    void add(E data) throws SerializeException, IOException;
 
     void remove() throws IOException;
 
-    byte[] peek() throws IOException, DeserializeException;
+    E peek() throws IOException, DeserializeException;
 
     int size();
 
     void close() throws IOException;
+
+    Collection<E> peek(int size);
 }
