0e0c8aeef7aba7005b3dee918d0e2fda584f2f3d
kushal.sharma <kushal.sharma@flipkart.com>
2016-04-12 10:38:05 +0530

    bug fix

--- batching/src/main/java/com/flipkart/batching/persistence/TapePersistenceStrategy.java
+++ batching/src/main/java/com/flipkart/batching/persistence/TapePersistenceStrategy.java
@@ -7,7 +7,6 @@ import com.flipkart.batching.exception.SerializeException;
 import com.flipkart.batching.tape.CustomObjectQueue;
 import com.flipkart.batching.tape.FileObjectQueue;
 import com.flipkart.batching.tape.InMemoryObjectQueue;
-import com.flipkart.batching.tape.ObjectQueue;
 
 import org.slf4j.LoggerFactory;
 
@@ -23,9 +22,18 @@ import java.util.Collection;
 
 public class TapePersistenceStrategy<E extends Data> extends InMemoryPersistenceStrategy<E> {
     private static final org.slf4j.Logger log = LoggerFactory.getLogger(TapePersistenceStrategy.class);
-    FileObjectQueue.Converter<E> converter;
+    private FileObjectQueue.Converter<E> converter;
     private String filePath;
     private CustomObjectQueue<E> queueFile;
+
+    /**
+     * Initialise Tape persistence strategy using this constructor. This takes in File Path and
+     * Serialization Strategy as parameters. {@link com.flipkart.batching.tape.FileObjectQueue.Converter} is
+     * initialized using the provided Serialization Strategy.
+     *
+     * @param filePath              path to save file
+     * @param serializationStrategy serialization strategy
+     */
 
     public TapePersistenceStrategy(String filePath, final SerializationStrategy<E, ? extends Batch> serializationStrategy) {
         this.filePath = filePath;
@@ -52,9 +60,22 @@ public class TapePersistenceStrategy<E extends Data> extends InMemoryPersistence
         };
     }
 
-    public ObjectQueue<E> getQueueFile() {
+    /**
+     * Returns the current Queue File
+     *
+     * @return queue file
+     */
+
+    public CustomObjectQueue<E> getQueueFile() {
         return queueFile;
     }
+
+    /**
+     * Adds a collection of data objects to {@link com.flipkart.batching.tape.QueueFile}
+     *
+     * @param dataCollection collection of data to be added to queue file
+     * @return true if queue file was edited
+     */
 
     @Override
     public boolean add(Collection<E> dataCollection) {
@@ -85,10 +106,9 @@ public class TapePersistenceStrategy<E extends Data> extends InMemoryPersistence
                 }
             } else if (dataCollection.contains(peekedData)) {
                 queueFile.remove();
-                return true;
             }
         }
-        return false;
+        return true;
     }
 
     @Override
