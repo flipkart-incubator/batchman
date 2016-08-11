4aca804e868e4c5d2a187ad9be04ec33c079c9ba
anirudh.r <anirudh.r@flipkart.com>
2016-08-11 10:47:41 +0530

    #109 Demo app changes

--- app/src/main/java/com/flipkart/batchdemo/CustomTagData.java
+++ app/src/main/java/com/flipkart/batchdemo/CustomTagData.java
@@ -24,8 +24,8 @@
 
 package com.flipkart.batchdemo;
 
-import com.flipkart.batching.data.Tag;
-import com.flipkart.batching.data.TagData;
+import com.flipkart.batchingcore.data.Tag;
+import com.flipkart.batchingcore.data.TagData;
 import com.google.gson.annotations.SerializedName;
 
 import org.json.JSONObject;
