13805fc4bd3937a40e6c0bae4886607be18d1f67
anirudh.r <anirudh.r@flipkart.com>
2017-01-18 13:36:21 +0530

    code format

--- batching-core/src/main/java/com/flipkart/batching/core/data/Tag.java
+++ batching-core/src/main/java/com/flipkart/batching/core/data/Tag.java
@@ -32,8 +32,7 @@ import java.io.Serializable;
  * Tag represents a group of {@link Data} objects to batch together.
  * It takes a {@link String} type ID as parameter in constructor.
  */
-public class
-Tag implements Serializable {
+public class Tag implements Serializable {
 
     private String id;
 
